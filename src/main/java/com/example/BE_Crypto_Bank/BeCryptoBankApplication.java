af4ef872f7f0ddf0f0df0118ac4d12fa75bc183a
thang0401 <khongbietthang0@gmail.com>
2025-01-14 23:09:12 +0700

    first commit

---
+++ src/main/java/com/example/BE_Crypto_Bank/BeCryptoBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.BE_Crypto_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeCryptoBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeCryptoBankApplication.class, args);
+	}
+
+}
