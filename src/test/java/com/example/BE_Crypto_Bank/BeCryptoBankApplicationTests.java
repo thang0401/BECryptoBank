b7a9f72e6413a9199a2ae483ad28738b6d80e4a3
thang0401 <khongbietthang0@gmail.com>
2025-01-14 23:09:12 +0700

    first commit

---
+++ src/test/java/com/example/BE_Crypto_Bank/BeCryptoBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BE_Crypto_Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeCryptoBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
