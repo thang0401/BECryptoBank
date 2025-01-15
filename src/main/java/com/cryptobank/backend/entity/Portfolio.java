8921cb64366d1e02ec71d5c1d45fa22ae1f9674f
TrongTin <trongtin.tranthai@gmail.com>
2025-01-15 20:12:06 +0700

    Add some entity

---
+++ src/main/java/com/cryptobank/backend/entity/Portfolio.java
@@ -0,0 +1,34 @@
+package com.cryptobank.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "portfolio")
+public class Portfolio {
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "status")
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "term_id")
+    private Term term;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "portfolio_id")
+    private PortfolioCategory category;
+
+}
