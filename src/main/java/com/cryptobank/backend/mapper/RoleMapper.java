24172bbce0bf4191b3aeca4793a50ba76aeed417
TrongTin <trongtin.tranthai@gmail.com>
2025-03-13 23:52:39 +0700

    new api for Role

---
+++ src/main/java/com/cryptobank/backend/mapper/RoleMapper.java
@@ -0,0 +1,16 @@
+package com.cryptobank.backend.mapper;
+
+import com.cryptobank.backend.DTO.RoleCreateRequest;
+import com.cryptobank.backend.DTO.RoleUpdateRequest;
+import com.cryptobank.backend.entity.Role;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface RoleMapper {
+
+    Role fromCreateRequest(RoleCreateRequest request);
+
+    Role fromUpdateRequest(RoleUpdateRequest request);
+
+}
