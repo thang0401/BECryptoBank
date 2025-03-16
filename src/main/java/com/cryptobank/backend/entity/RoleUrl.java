3c61b8ba0b9bdd95ff9349e1f3e24ac3d66bdee1
TrongTin <trongtin.tranthai@gmail.com>
2025-03-16 18:22:36 +0700

    fix RoleUrl api and add RoleUrlDTO

--- src/main/java/com/cryptobank/backend/entity/RoleUrl.java
+++ src/main/java/com/cryptobank/backend/entity/RoleUrl.java
@@ -1,20 +1,27 @@
 package com.cryptobank.backend.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "role_url")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Table(
+    name = "role_url",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"role_id", "function_url"})
+    }
+)
 public class RoleUrl extends BaseEntity {
 
-    @Column(name = "function_url", columnDefinition = "TEXT")
+    @Column(name = "function_url", columnDefinition = "TEXT", nullable = false)
     private String functionUrl;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "role_id")
+    @JoinColumn(name = "role_id", nullable = false)
     private Role role;
 
 }
