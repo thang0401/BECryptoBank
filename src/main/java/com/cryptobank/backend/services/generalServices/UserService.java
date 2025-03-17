2053b07691825a0c75978cf75721b4642e022096
Pham Van Phong <vanphongdz435@gmail.com>
2025-03-17 21:19:00 +0700

    fix user

--- src/main/java/com/cryptobank/backend/services/generalServices/UserService.java
+++ src/main/java/com/cryptobank/backend/services/generalServices/UserService.java
@@ -5,6 +5,8 @@ import com.cryptobank.backend.exception.AlreadyExistException;
 import com.cryptobank.backend.repository.UserDAO;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,106 +14,127 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
+@RequiredArgsConstructor
 @Service
 public class UserService {
 
-    private final UserDAO ;
-    private final EmailService emailService;
+	private EmailService emailService;
 
-    public User get(String id) {
-        return repository.findById(id).orElseThrow(() -> new RuntimeException("User id " + id + " not found"));
-    }
+	@Autowired
+	private UserDAO repository;
 
-    public List<User> getAll() {
-        return repository.findAll();
-    }
+	public User get(String id) {
+		return repository.findById(id).orElseThrow(() -> new RuntimeException("User id " + id + " not found"));
+	}
 
-    public Page<User> getAll(int page, int size) {
-        return repository.findAll(PageRequest.of(page, size));
-    }
+	public List<User> getAll() {
+		return repository.findAll();
+	}
 
-    public void delete(String id) {
-        User user = get(id);
-        repository.delete(user);
-    }
+	public Page<User> getAll(int page, int size) {
+		return repository.findAll(PageRequest.of(page, size));
+	}
 
-    public void update(String id, User user) {
-        if (repository.existsById(id)) {
-            repository.save(user);
-        }
-    }
+	public void delete(String id) {
+		User user = get(id);
+		repository.delete(user);
+	}
 
-    public User save(User entity) {
-        User user = repository.findByEmail(entity.getEmail());
-        if (user != null) {
-            throw new AlreadyExistException("User with email " + entity.getEmail() + " already exists");
-        } else {
-            return repository.save(entity);
-        }
-    }
+	public void update(String id, User user) {
+		if (repository.existsById(id)) {
+			repository.save(user);
+		}
+	}
 
-    public User getEmail(String email) {
-        return repository.findByEmail(email);
-    }
+	public User save(User entity) {
+		User user = repository.findByEmail(entity.getEmail());
+		if (user != null) {
+			throw new AlreadyExistException("User with email " + entity.getEmail() + " already exists");
+		} else {
+			return repository.save(entity);
+		}
+	}
 
-    public List<User> getName(String name) {
-        return repository.findByName(name);
-    }
+	public User getEmail(String email) {
+		return repository.findByEmail(email);
+	}
 
-    public boolean existsByEmail(String email) {
-        return repository.existsByEmail(email);
-    }
+	public List<User> getName(String name) {
+		return repository.findByName(name);
+	}
 
-    public boolean existsByPhoneNumber(String phoneNumber) {
-        return repository.existsByPhoneNumber(phoneNumber);
-    }
+	public boolean existsByEmail(String email) {
+		return repository.existsByEmail(email);
+	}
 
-    public boolean existsByIdCardNumber(String cardNumber) {
-        return repository.existsByIdCardNumber(cardNumber);
-    }
+	public boolean existsByPhoneNumber(String phoneNumber) {
+		return repository.existsByPhoneNumber(phoneNumber);
+	}
 
-    private String generateResetCode() {
-        int randomCode = (int) (Math.random() * 900000) + 100000;  // Tạo mã 6 chữ số
-        return String.valueOf(randomCode);
-    }
+	public boolean existsByIdCardNumber(String cardNumber) {
+		return repository.existsByIdCardNumber(cardNumber);
+	}
 
-    public void requestResetPassword(String email, HttpSession session) {
-        getEmail(email);
+	private String generateResetCode() {
+		int randomCode = (int) (Math.random() * 900000) + 100000; // Tạo mã 6 chữ số
+		return String.valueOf(randomCode);
+	}
 
-        // Tạo mã xác thực ngẫu nhiên 6 chữ số
-        String resetCode = generateResetCode();
-        session.setAttribute("OTP", resetCode);
-        session.setAttribute("time", LocalDateTime.now().plusMinutes(15));// Mã hết hạn sau 15 phút
+	public void requestResetPassword(String email, HttpSession session) {
+		getEmail(email);
 
-        // Gửi email với mã xác thực
-        emailService.sendResetPasswordEmail(email, resetCode);
-    }
+		// Tạo mã xác thực ngẫu nhiên 6 chữ số
+		String resetCode = generateResetCode();
+		session.setAttribute("OTP", resetCode);
+		session.setAttribute("time", LocalDateTime.now().plusMinutes(15));// Mã hết hạn sau 15 phút
 
-    public void resetPassword(String email, String resetCode, String newPassword, HttpSession session) {
-        // Tìm người dùng qua email
-        User user = getEmail(email);
+		// Gửi email với mã xác thực
+		emailService.sendResetPasswordEmail(email, resetCode);
+	}
 
-        // Kiểm tra mã resetCode có đúng không
-        String OTP = (String) session.getAttribute("OTP");
-        if (!OTP.equals(resetCode)) {
-            throw new RuntimeException("Mã xác thực không hợp lệ");
-        }
+	public void resetPassword(String email, String resetCode, String newPassword, HttpSession session) {
+		// Tìm người dùng qua email
+		User user = getEmail(email);
 
-        // Kiểm tra mã resetCode có hết hạn hay không
-        LocalDateTime timeLimit = (LocalDateTime)session.getAttribute("time");
-        if (!timeLimit.isBefore(LocalDateTime.now())) {
-            throw new RuntimeException("Mã xác thực đã hết hạn");
-        }
+		// Kiểm tra mã resetCode có đúng không
+		String OTP = (String) session.getAttribute("OTP");
+		if (!OTP.equals(resetCode)) {
+			throw new RuntimeException("Mã xác thực không hợp lệ");
+		}
 
-        // Mã xác thực hợp lệ, tiến hành thay đổi mật khẩu
-        // Mã hóa mật khẩu mới
-        String encodedPassword = new BCryptPasswordEncoder().encode(newPassword);
-        User userChangePass=repository.findByEmail(email);
-        userChangePass.setPassword(encodedPassword);
+		// Kiểm tra mã resetCode có hết hạn hay không
+		LocalDateTime timeLimit = (LocalDateTime) session.getAttribute("time");
+		if (!timeLimit.isBefore(LocalDateTime.now())) {
+			throw new RuntimeException("Mã xác thực đã hết hạn");
+		}
 
-        // Lưu thông tin người dùng mới với mật khẩu đã thay đổi vào database
-        save(userChangePass);
-    }
+		// Mã xác thực hợp lệ, tiến hành thay đổi mật khẩu
+		// Mã hóa mật khẩu mới
+		String encodedPassword = new BCryptPasswordEncoder().encode(newPassword);
+		User userChangePass = repository.findByEmail(email);
+		userChangePass.setPassword(encodedPassword);
+
+		// Lưu thông tin người dùng mới với mật khẩu đã thay đổi vào database
+		save(userChangePass);
+	}
+
+	// Lấy số dư USDC của user
+	public Double getUserBalance(String userId) {
+		Optional<User> user = repository.findById(userId);
+		// return user.map(User::getUsdcBalance).orElse(0.0);
+		return 0.0;
+	}
+
+	// Giảm số dư USDC sau khi rút tiền thành công
+	public void decreaseUserBalance(String userId, Double amount) 
+	{
+		Optional<User> userOpt = repository.findById(userId);
+//		userOpt.ifPresent(user -> {
+//			user.setUsdcBalance(user.getUsdcBalance() - amount);
+//			repository.save(user);
+//		});
+	}
 
 }
