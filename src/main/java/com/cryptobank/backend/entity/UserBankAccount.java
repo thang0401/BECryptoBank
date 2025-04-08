d22709ac576df10db2c25df37d1c40e0ae6670ff
Pham Van Phong <vanphongdz435@gmail.com>
2025-04-08 20:14:00 +0700

    code withDraw payos

---
+++ src/main/java/com/cryptobank/backend/entity/UserBankAccount.java
@@ -0,0 +1,43 @@
+package com.cryptobank.backend.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user_bank_account")
+public class UserBankAccount {
+	 @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+
+	    @ManyToOne(fetch = FetchType.LAZY)
+	    @JoinColumn(name = "user_id", nullable = false)
+	    private User user;
+
+	    @Column(name = "bank_name", nullable = false, length = 255)
+	    private String bankName;
+
+	    @Column(name = "account_number", nullable = false, unique = true, length = 50)
+	    private String accountNumber;
+
+	    @Column(name = "account_holder_name", nullable = false, length = 255)
+	    private String accountHolderName;
+
+	    @Column(name = "bank_code", nullable = false, length = 50)
+	    private String bankCode;
+
+	    @Column(name = "created_at", nullable = false, updatable = false)
+	    private LocalDateTime createdAt = LocalDateTime.now();
+
+	    @Column(name = "updated_at")
+	    private LocalDateTime updatedAt = LocalDateTime.now();
+
+	    @PreUpdate
+	    protected void onUpdate() {
+	        updatedAt = LocalDateTime.now();
+	    }
+}
