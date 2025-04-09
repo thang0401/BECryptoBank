f3f102b2594afd4b56186a2fea2e5c84ab2b3ca0
TrongTin <trongtin.tranthai@gmail.com>
2025-04-09 23:29:10 +0700

    add avatar to user

--- src/main/java/com/cryptobank/backend/controller/UploadImageController.java
+++ src/main/java/com/cryptobank/backend/controller/UploadImageController.java
@@ -1,9 +1,13 @@
 package com.cryptobank.backend.controller;
 
+import com.cryptobank.backend.entity.User;
 import com.cryptobank.backend.services.MinioService;
+import com.cryptobank.backend.services.UserService;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class UploadImageController {
 
     private final MinioService minioService;
+    private final UserService userService;
 
     @PostMapping
     public String uploadImage(@RequestParam MultipartFile file) throws IOException {
@@ -25,6 +30,14 @@ public class UploadImageController {
     @DeleteMapping
     public void deleteImage(@RequestParam String file) {
         minioService.deleteFile(file);
+    }
+
+    @PostMapping(value = "/{userId}", produces = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public User addAvatarToUser(@PathVariable String userId, @RequestParam MultipartFile file) throws IOException {
+        User user = userService.getUserKYC(userId);
+        String fileUrl = minioService.uploadFile(file.getOriginalFilename(), file.getInputStream(), file.getContentType());
+        user.setAvatarUrl(fileUrl);
+        return user;
     }
 
 }
