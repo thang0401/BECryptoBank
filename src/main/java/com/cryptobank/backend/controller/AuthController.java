1246ac6646098736eed2a3ca46db908401bd3230
Pham Van Phong <vanphongdz435@gmail.com>
2025-04-19 20:47:58 +0700

    done deposit and webhook but still have payment and withdraw

--- src/main/java/com/cryptobank/backend/controller/AuthController.java
+++ src/main/java/com/cryptobank/backend/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.cryptobank.backend.controller;
 
-import com.cryptobank.backend.exception.AuthException;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.List;
@@ -9,27 +8,37 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.cryptobank.backend.DTO.*;
+import com.cryptobank.backend.DTO.AuthResponse;
+import com.cryptobank.backend.DTO.GoogleLoginRequest;
+import com.cryptobank.backend.DTO.LoginRequestAuth;
+import com.cryptobank.backend.DTO.NameSplit;
+import com.cryptobank.backend.DTO.OtpRequest;
+import com.cryptobank.backend.DTO.ResetPasswordRequest;
+import com.cryptobank.backend.DTO.UserAuthResponse;
 import com.cryptobank.backend.entity.DeviceInfo;
 import com.cryptobank.backend.entity.User;
 import com.cryptobank.backend.entity.UserOtp;
+import com.cryptobank.backend.exception.AuthException;
 import com.cryptobank.backend.repository.DeviceInforDAO;
 import com.cryptobank.backend.repository.UserDAO;
 import com.cryptobank.backend.repository.UserOtpRepository;
 import com.cryptobank.backend.services.AuthService;
 import com.cryptobank.backend.services.UserService;
 
-import jakarta.security.auth.message.AuthException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import eu.bitwalker.useragentutils.UserAgent;
 import eu.bitwalker.useragentutils.Browser;
 import eu.bitwalker.useragentutils.OperatingSystem;
+import eu.bitwalker.useragentutils.UserAgent;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/auth")
