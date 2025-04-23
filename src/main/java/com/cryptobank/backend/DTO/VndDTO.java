f93f1c55f533a227e4b7ea8be27b7e114741ed09
Pham Van Phong <vanphongdz435@gmail.com>
2025-04-23 18:08:34 +0700

    add usdc exchange to vnd and other

---
+++ src/main/java/com/cryptobank/backend/DTO/VndDTO.java
@@ -0,0 +1,14 @@
+package com.cryptobank.backend.DTO;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VndDTO {
+	private BigDecimal vnd;
+}
