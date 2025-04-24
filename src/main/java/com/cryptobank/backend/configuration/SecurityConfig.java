4c7cf68c1951aed1b68919019e5ee29b45a79732
thang0401 <khongbietthang0@gmail.com>
2025-04-24 09:23:16 +0700

    fig security config

--- src/main/java/com/cryptobank/backend/configuration/SecurityConfig.java
+++ src/main/java/com/cryptobank/backend/configuration/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.cryptobank.backend.configuration;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,6 +16,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -21,8 +27,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         return http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> {
                     request.anyRequest().permitAll();
                 })
@@ -37,6 +43,25 @@ public class SecurityConfig {
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder);
         return new ProviderManager(authProvider);
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        List<String> allowedOrigins = new ArrayList<>(List.of(
+                "http://localhost:3000",
+                "http://localhost:3001",
+                "http://localhost:8000",
+                "https://admin-crypto-bank.vercel.app",
+                "https://client-crypto-bank.vercel.app",
+                "https://be-crypto-depot.name.vn"));
+//        allowedOrigins.addAll(List.of(corsUrl.split(",")));
+        configuration.setAllowedOrigins(allowedOrigins);
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
     
 }
--- src/main/java/com/cryptobank/backend/exception/AuthException.java
+++ src/main/java/com/cryptobank/backend/exception/AuthException.java
@@ -1,8 +1,19 @@
 package com.cryptobank.backend.exception;
 
-
 public class AuthException extends RuntimeException {
+    private final String userId;
+
     public AuthException(String message) {
         super(message);
+        this.userId = null;
+    }
+
+    public AuthException(String message, String userId) {
+        super(message);
+        this.userId = userId;
+    }
+
+    public String getUserId() {
+        return userId;
     }
 }
