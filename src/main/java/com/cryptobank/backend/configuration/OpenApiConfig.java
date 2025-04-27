21046e571948cb6b4bb880191d2acca0639a5b4f
TrongTin <trongtin.tranthai@gmail.com>
2025-04-27 16:04:37 +0700

    fix Swagger

--- src/main/java/com/cryptobank/backend/configuration/OpenApiConfig.java
+++ src/main/java/com/cryptobank/backend/configuration/OpenApiConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +16,7 @@ public class OpenApiConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
             .info(new Info().title("CryptoBank API").version("1.0"))
+            .servers(List.of(new Server().url("https://be-crypto-depot.name.vn")))
             .components(new Components().addSecuritySchemes("Bearer Authorization", new SecurityScheme()
                 .name("Bearer Authorization")
                 .type(SecurityScheme.Type.HTTP)
