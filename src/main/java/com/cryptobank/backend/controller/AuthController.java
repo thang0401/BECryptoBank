a094913aa61dd700935279d8b9184fcc209c05f3
Pham Van Phong <vanphongdz435@gmail.com>
2025-04-28 21:21:38 +0700

    done auth

--- src/main/java/com/cryptobank/backend/controller/AuthController.java
+++ src/main/java/com/cryptobank/backend/controller/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,6 +41,7 @@ import eu.bitwalker.useragentutils.UserAgent;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:5500"}, allowCredentials = "true")
 public class AuthController {
 
     @Autowired
