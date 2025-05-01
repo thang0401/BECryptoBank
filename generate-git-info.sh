6500ec558237d24a27b1fbd1d42728d8d0bc11ed
TrongTin <trongtin.tranthai@gmail.com>
2025-05-01 18:50:53 +0700

    Update script

--- generate-git-info.sh
+++ generate-git-info.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-COMMIT_HASH=$(git log --no-merges -n 1 --pretty=format:"%H")
-COMMIT_MESSAGE=$(git log --no-merges -n 1 --pretty=format:"%s")
-COMMIT_AUTHOR=$(git log --no-merges -n 1 --pretty=format:"%an")
-COMMIT_TIME=$(git log --no-merges -n 1 --pretty=format:"%ci")
+COMMIT_HASH=$(git log origin/main --no-merges -n 1 --pretty=format:"%H")
+COMMIT_MESSAGE=$(git log origin/main --no-merges -n 1 --pretty=format:"%s")
+COMMIT_AUTHOR=$(git log origin/main --no-merges -n 1 --pretty=format:"%an")
+COMMIT_TIME=$(git log origin/main --no-merges -n 1 --pretty=format:"%ci")
 
 update_env() {
     local key="$1"
@@ -21,3 +21,9 @@ update_env "GIT_COMMIT_ID_FULL" "$COMMIT_HASH"
 update_env "GIT_COMMIT_MESSAGE_FULL" "$COMMIT_MESSAGE"
 update_env "GIT_COMMIT_AUTHOR_NAME" "$COMMIT_AUTHOR"
 update_env "GIT_COMMIT_TIME" "$COMMIT_TIME"
+
+echo "== Git Commit Info =="
+echo "Full Commit: $COMMIT_HASH"
+echo "Message: $COMMIT_MESSAGE"
+echo "Author: $COMMIT_AUTHOR"
+echo "Time: $COMMIT_TIME"
